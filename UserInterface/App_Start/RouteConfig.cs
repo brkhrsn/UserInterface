d5fc5fff2660325721fb7acf10937cbaaf7c50ae
brkhrsn <30520195+brkhrsn@users.noreply.github.com>
2017-11-15 11:59:28 +0300

    Initial commit

---
+++ UserInterface/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UserInterface
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "UserInterface", action = "Create", id = UrlParameter.Optional } // Burada controller ve action'a değer vererek başlangıçta yazdığımız sayfaların çalışmasını sağladık.
+            );
+        }
+    }
+}
---
+++ UserInterface/Controllers/UserInterfaceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Providers.Entities;
+using UserInterface.Models;
+
+namespace UserInterface.Controllers
+{
+    //Burada kuyruk yapısının Enqueue methodunu override ederek değiştirdik ve kuyruktan çıkan ilk nesneyi mail olarak attık.
+    public override void Enqueue(object obj)
+    {
+    base.Enqueue(obj);
+    OnChanged(SendMail());
+    }
+    public class UserInterfaceController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Sayfayı sadece görüntülemek istediğimde yani post olmadığında, bu kısım çalışacak.
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Buraya HttpPost eklememizin nedeni, bu action sadece post olunca çalışacak olması.
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            //Burada kuyruk nesnesini oluşturduk ve kuyruğa davetli kullanıcının email adresini ekledik.
+            Queue sira = new Queue();
+            sira.Enqueue(form["DavetEmail"].Trim());
+
+            //Models klasörüne ADO.NET Entity Data Model ekledik ve adına FormDB dedik. 
+            //Normalde ADO.NET Entity Data Model eklerken "Generate From Database" seçeneğini seçeriz ve önceden tasarladığımız veritabanını seçeriz
+            //Ancak burada ben önceden database oluşturmadığım için Empty seçeneğini seçtim.
+         
+            FormDBEntities db = new FormDBEntities();
+            Users model = new User();
+            model.UserName = form["Name"].Trim();
+            model.Surname = form["SurName"].Trim();
+            model.Email = form["Email"].Trim();
+            db.Users.add(model);
+            db.SaveChanges();
+
+            return View();
+        }
+    }
+}
